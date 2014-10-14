51aa43f5c06485819e3982448eb036d89fcb5981
Anil Shrestha <jesnilshrestha03@hotmail.com>
2014-10-13 22:33:22 -0700

    Main Application folder for Product Management Application

---
+++ productManagement/app/app.js
@@ -0,0 +1,7 @@
+
+(function () {
+	"use strict"; /* catch common Javascript error*/
+	var app = angular.module("productManagement", 
+							["common.services",
+							"productResourceMock"]);
+} ()); /* () is to execute the function  by self*/
